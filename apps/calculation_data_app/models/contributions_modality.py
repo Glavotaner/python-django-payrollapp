56e5d6e01b04b4f0618d4a72874e87985bef4fef
Marin Glavaš <mglavas@spin.hr>
2020-10-13 13:59:12 +0200

    Added and translated help text in contributions app

--- apps/calculation_data_app/models/contributions_modality.py
+++ apps/calculation_data_app/models/contributions_modality.py
@@ -6,18 +6,18 @@ from django.utils.translation import gettext_lazy as _
 class ContributionsModality(models.Model):
 
     modality_mark = models.CharField(
-        verbose_name=_('Modality mark'), unique=True, max_length=10)
+        verbose_name=_('Modality mark'), unique=True, max_length=10, help_text = _('Up to 10 characters long textual ID of this modality'))
 
     pension_fund_min_base = models.FloatField(
-        verbose_name= _('Pension fund minimal base'))
+        verbose_name= _('Pension fund minimal base'), help_text = _("The year's legal minimum pension fund base. Use '.' as decimal point"), default = 3321.96)
 
     pension_fund_gen = models.FloatField(
-        verbose_name= _('Pension fund 1'), default=0.15)
+        verbose_name= _('Pension fund - generational'), default=0.15, help_text = _('Legal rate for the generational pension fund. Expressed as a decimal number'))
     pension_fund_ind = models.FloatField(
-        verbose_name= _('Pension fund 2'), default=0.05)
+        verbose_name= _('Pension fund - individual'), default=0.05, help_text = _('Legal rate for the individual pension fund. Expressed as a decimal number'))
 
     health_insurance = models.FloatField(
-        verbose_name= _('Health fund'), default=0.165)
+        verbose_name= _('Health fund'), default=0.165, help_text = _('Legal rate for the health insurance fund. Expressed as a decimal number'))
 
     class Meta:
 
